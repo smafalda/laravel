a48db18b67ad1cbfa073378bddf44599a5b2cb66
salomao <salomao.mafalda@rjadv.com>
2020-04-07 16:13:18 -0500

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,109 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Monolog\Handler\RotatingFileHandler;
 
+
+#Login
+Route::get('/login', function(){
+    return "hey hey hey";
+})->name('login');
+
+
+
+#GROUP ROUTE - ADMINISTRATOR
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Route::middleware([])->group(function(){  #define e metodo de auth
+
+    Route::prefix('admin')->group(function(){  #define o prefixo admin 'admin/dashboard'
+
+        Route::namespace('Admin')->group(function(){
+
+            Route::name('admin.')->group(function(){
+
+                Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+        
+                Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+                
+                Route::get('/produtos', 'TesteController@teste')->name('produtos');
+        
+                Route::redirect('/', function(){
+                    return redirect()->route('admin.dashboard');
+                })->name('home');
+
+            });
+
+        });
+
+    });
+
+});
 */
+
+Route::group([
+        'middleware'=>[],
+        'prefix'=>'admin',
+        'namespace'=>'Admin',
+        'name'=> 'admin.'
+    ], function(){
+        Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+        
+        Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+                
+        Route::get('/produtos', 'TesteController@teste')->name('produtos');
+        
+        Route::get('/', function(){
+            return redirect()->route('admin.dashboard');
+        })->name('home');   
+});
+
+
+
+#MOstrando view
+Route::view('/view', 'welcome');
+Route::any('/any', function () {
+    return 'any';
+});
+
+Route::get('/nome-url', function(){
+    return "hey hey hey";
+})->name('url.name');
+
+Route::get('redirect3', function(){
+   # return redirect('/nome-url');
+   return redirect()->route('url.name');
+});
+
+
+
+#aceita os metodos especifivcados {get, post etc}
+Route::match(['get', 'post'], '/any', function () {
+    return 'any';
+});
+#rotas padrão
+Route::get('/contato', function () {
+    return view('contato');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/categoria/{flag}/posts', function ($flag) {
+    return "Produtos da categoria: {$flag}";
+});
+
+
+#redirecionamento de rotas
+
+Route::redirect('redirect1', 'redirect2');
+/*
+Route::get('redirect1', function(){
+    return redirect('/redirect2');
+});
+*/
+Route::get('redirect2', function(){
+    return 'Redirect 02';
+});
+
